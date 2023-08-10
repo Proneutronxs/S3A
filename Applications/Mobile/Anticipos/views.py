88c79f32e365e4beff45261b65755d944a705ae4
Josue <fumanny@hotmail.com>
2023-08-10 10:38:09 -0300

    version 1.01

--- Applications/Mobile/Anticipos/views.py
+++ Applications/Mobile/Anticipos/views.py
@@ -50,8 +50,8 @@ def insert_anticipos(request):
             contenido = 'Se cargaron anticipos de las siguientes personas: \n \n' + ', \n'.join(listado) + '.'
             asunto = 'Carga de Anticipos.'
             listadoCorreos = correosChacras()
-            for correo in listadoCorreos:
-                enviarCorreo(asunto,contenido,correo)
+            # for correo in listadoCorreos:
+            #     enviarCorreo(asunto,contenido,correo)
 
             estado = "E"
             insertaRegistro(usuario, fechaHora, registro, estado)
