f02756cfb26f7af12b58dbde10d73d28a84592ea
Josue <fumanny@hotmail.com>
2023-12-14 15:14:30 -0300

    advance

--- Applications/Logistica/folderViews/SeguimientosViajes/seguimientos.py
+++ Applications/Logistica/folderViews/SeguimientosViajes/seguimientos.py
@@ -117,8 +117,8 @@ def listadoViajes(request):
                                                     TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento ON PedidoFlete.IdPedidoFlete = TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento.IdAsignacion
                             WHERE        (PedidoFlete.Estado = 'A')
                                         AND (CONVERT(DATE, TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento.FechaHora) >= DATEADD(DAY, - 2, CONVERT(DATE, GETDATE())))
-                                        AND TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento.Estado IN ('S','F) 
-                            ORDER BY TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento.Actuallizacion DESC"""
+                                        AND TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento.Estado IN ('S','F') 
+                            ORDER BY TRESASES_APLICATIVO.dbo.Logistica_Camiones_Seguimiento.Actuallizacion DESC """
                     cursor.execute(sql)
                     consulta = cursor.fetchall()
                     if consulta:
