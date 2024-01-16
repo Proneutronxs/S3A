75c161fbbcc7b8d7ccd9cab257a3e5dd8ad74640
Josue <fumanny@hotmail.com>
2024-01-16 08:04:13 -0300

    advance

--- Applications/Mobile/Anticipos/views.py
+++ Applications/Mobile/Anticipos/views.py
@@ -81,7 +81,7 @@ def insert_anticipos(request):
                     LegajoNombre = obtieneNombres(item)
                     datosLegajo = LegajoNombre 
                     listadoJson.append(datosLegajo)
-                nota = 'Se cargaron anticipos. Las siguientes personas ya tiene un Adelanto de Sueldo este Mes: \n \n' + ', \n'.join(listadoJson) + '.'
+                nota = 'Se cargaron anticipos. Las siguientes personas ya tiene un Adelanto de Sueldo y/ó Ingreso este Mes: \n \n' + ', \n'.join(listadoJson) + '.'
                 estado = "E"
                 insertaRegistro(usuario, fechaHora, registro, estado)
                 return JsonResponse({'Message': 'Success', 'Nota': nota})  
