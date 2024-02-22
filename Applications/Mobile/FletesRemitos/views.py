2c1062d2bfd48668be068970fb344327f10b575a
Josue <fumanny@hotmail.com>
2024-02-21 21:15:46 -0300

    advance

--- Applications/Mobile/FletesRemitos/views.py
+++ Applications/Mobile/FletesRemitos/views.py
@@ -1078,35 +1078,41 @@ def actualizaEstadoPosicion(request):
                 
             if Columna == 'Final':
                 if verificaBinLleno(IdAsignacion) == 'RAU':
-                    if verificaLote(IdAsignacion):
-                        with connections['TRESASES_APLICATIVO'].cursor() as cursor:
-                            sql = f"UPDATE Logistica_Camiones_Seguimiento SET {Columna} = %s, HoraFinal = GETDATE(), Estado = 'F', Actualizacion = GETDATE() WHERE IdAsignacion = %s AND Estado = 'S' "
-                            cursor.execute(sql, [Valor, IdAsignacion])           
+                    try:
+                        if verificaLote(IdAsignacion):
+                            with connections['TRESASES_APLICATIVO'].cursor() as cursor:
+                                sql = f"UPDATE Logistica_Camiones_Seguimiento SET {Columna} = %s, HoraFinal = GETDATE(), Estado = 'F', Actualizacion = GETDATE() WHERE IdAsignacion = %s AND Estado = 'S' "
+                                cursor.execute(sql, [Valor, IdAsignacion])           
 
-                            sqlUpdate = """UPDATE Logistica_Estado_Camiones 
-                                        SET Libre = 'S', Actualizado = GETDATE() 
-                                        WHERE NombreChofer = %s 
-                                        AND NOT EXISTS (
-                                            SELECT 1 
-                                            FROM Logistica_Camiones_Seguimiento 
-                                            WHERE Chofer = %s AND Estado = 'S'
-                                        )""" 
-                            cursor.execute(sqlUpdate, [Chofer,Chofer])     
+                                sqlUpdate = """UPDATE Logistica_Estado_Camiones 
+                                            SET Libre = 'S', Actualizado = GETDATE() 
+                                            WHERE NombreChofer = %s 
+                                            AND NOT EXISTS (
+                                                SELECT 1 
+                                                FROM Logistica_Camiones_Seguimiento 
+                                                WHERE Chofer = %s AND Estado = 'S'
+                                            )""" 
+                                cursor.execute(sqlUpdate, [Chofer,Chofer])     
 
-                            sqlDelete = "DELETE Logistica_Campos_Temporales WHERE IdAsignacion = %s"
-                            cursor.execute(sqlDelete, [IdAsignacion])
+                                sqlDelete = "DELETE Logistica_Campos_Temporales WHERE IdAsignacion = %s"
+                                cursor.execute(sqlDelete, [IdAsignacion])
 
-                            cursor.execute("SELECT @@ROWCOUNT AS AffectedRows")
-                            affected_rows = cursor.fetchone()[0]
+                                cursor.execute("SELECT @@ROWCOUNT AS AffectedRows")
+                                affected_rows = cursor.fetchone()[0]
 
-                        if affected_rows > 0:
-                            return JsonResponse({'Message': 'Success', 'Nota': 'F', 'Estado':textUbicacion(Chofer)})
+                            if affected_rows > 0:
+                                return JsonResponse({'Message': 'Success', 'Nota': 'F', 'Estado':textUbicacion(Chofer)})
+                            else:
+                                return JsonResponse({'Message': 'Error', 'Nota': 'No se pudo Finalizar', 'Estado':textUbicacion(Chofer)})
+                                    
                         else:
-                            return JsonResponse({'Message': 'Error', 'Nota': 'No se pudo Finalizar', 'Estado':textUbicacion(Chofer)})
-                        
-                    else:
-                        insertar_registro_error_sql("ELSE BIN NO INGRESO","actualizaEstadoPosicion","Aplicacion","error")
-                        return JsonResponse({'Message': 'Error', 'Nota': 'EL VIAJE NO SE PUEDE FINALIZAR SI NO INGRESÓ EN BÁSCULA.', 'Estado':textUbicacion(Chofer)})                       
+                            insertar_registro_error_sql("ELSE BIN NO INGRESO","actualizaEstadoPosicion","Aplicacion","error")
+                            return JsonResponse({'Message': 'Error', 'Nota': 'EL VIAJE NO SE PUEDE FINALIZAR SI NO INGRESÓ EN BÁSCULA.', 'Estado':textUbicacion(Chofer)})
+                    except Exception as e:
+                        # Manejo de la excepción
+                        mensaje_error = str(e)
+                        insertar_registro_error_sql("ELSE BIN NO INGRESO","actualizaEstadoPosicion","Aplicacion",mensaje_error)
+                        return JsonResponse({'Message': 'Error', 'Nota': mensaje_error})                    
                         
                 else:
                     with connections['TRESASES_APLICATIVO'].cursor() as cursor:
