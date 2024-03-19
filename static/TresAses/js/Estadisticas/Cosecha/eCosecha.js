68c5afefcecd568952138bed1ec6d6f37a36adab
Josue <fumanny@hotmail.com>
2024-03-18 23:33:44 -0300

    advance

--- static/TresAses/js/Estadisticas/Cosecha/eCosecha.js
+++ static/TresAses/js/Estadisticas/Cosecha/eCosecha.js
@@ -9,130 +9,6 @@ window.addEventListener("load", async () =>{
 });
 
 
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     fechaActual();
-//     // Obtener el contexto del lienzo
-//     const ctx = document.getElementById('line-chart').getContext('2d');
-
-//     // Definir los datos del gráfico
-//     const data = {
-//         labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes','Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
-//         datasets: [{
-//             label: 'PERA',
-//             data: [1234, 1532, 1224, 1562, 1538, 1238,1586,1485,1234, 1532],
-//             borderColor: 'blue',
-//             backgroundColor: 'transparent',
-//             borderWidth: 2
-//         },
-//         {
-//             label: 'MANZANA',
-//             data: [1254, 1632, 1124, 962, 1138, 1038,1786,1885, 1124, 962],
-//             borderColor: 'green',
-//             backgroundColor: 'transparent',
-//             borderWidth: 2
-//         }]
-//     };
-
-//     // Configurar las opciones del gráfico
-//     const options = {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     };
-
-//     // Crear el gráfico de líneas
-//     const lineChart = new Chart(ctx, {
-//         type: 'line',
-//         data: data,
-//         options: options
-//     });
-// });
-
-
-
-
-
-// const muestraResultados = async () => {
-//     openProgressBar();
-//     try {
-//         const form = document.getElementById("formEstadisticas");
-//         const formData = new FormData(form);
-
-//         const options = {
-//             method: 'POST',
-//             headers: {},
-//             body: formData
-//         };
-//         const response = await fetch("ver-estadisticas/", options);
-//         const dato = await response.json();
-//         if(dato.Message === "Success") {
-//             closeProgressBar();
-//             const ctx = document.getElementById('line-chart').getContext('2d');
-
-//             const dataDatos = JSON.parse(dato.Datos);
-
-//             console.log(dataDatos);
-            
-//             const tipoFruta = Object.keys(dataDatos)[0]; // Obtener el primer (y único) tipo de fruta
-//             const labels = dataDatos[tipoFruta].map(entry => entry.Fecha);
-//             const datos = dataDatos[tipoFruta].map(entry => parseInt(entry.Cantidad));
-
-//             // Crear el objeto de datos para el gráfico
-//             const data = {
-//                 labels: labels,
-//                 datasets: [{
-//                     label: tipoFruta, // Usar el tipo de fruta como etiqueta del dataset
-//                     data: datos,
-//                     borderColor: 'blue',
-//                     backgroundColor: 'transparent',
-//                     borderWidth: 2
-//                 }]
-//             };
-            
-//             const options = {
-//             scales: {
-//                 y: {
-//                 beginAtZero: true
-//                 }
-//             }
-//             };
-            
-//             const lineChart = new Chart(ctx, {
-//             type: 'line',
-//             data: data,
-//             options: options
-//             });
-
-
-//         } else {
-//             closeProgressBar();
-//             const nota = dato.Nota;
-//             const color = "red";
-//             mostrarInfo(nota, color);
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         closeProgressBar();
-//         const nota = "Se produjo un error al procesar la solicitud.";
-//         const color = "red";
-//         mostrarInfo(nota, color);
-//     }
-// };
-
-
-
 // const muestraResultados = async () => {
 //     openProgressBar();
 //     try {
@@ -164,11 +40,14 @@ window.addEventListener("load", async () =>{
 //                 const labels = dataDatos[tipoFruta].map(entry => entry.Fecha);
 //                 const datos = dataDatos[tipoFruta].map(entry => parseInt(entry.Cantidad));
 
+//                 // Sumar la cantidad de bins
+//                 const totalBins = datos.reduce((acc, curr) => acc + curr, 0);
+
 //                 // Crear el objeto de datos para el gráfico
 //                 const data = {
 //                     labels: labels,
 //                     datasets: [{
-//                         label: tipoFruta, // Usar el tipo de fruta como etiqueta del dataset
+//                         label: `${tipoFruta} (${totalBins} - Bins)`, // Tipo de fruta con la cantidad de bins
 //                         data: datos,
 //                         borderColor: 'blue',
 //                         backgroundColor: 'transparent',
@@ -274,6 +153,18 @@ const muestraResultados = async () => {
                             y: {
                                 beginAtZero: true
                             }
+                        },
+                        plugins: {
+                            datalabels: {
+                                display: function(context) {
+                                    return context.dataset.data[context.dataIndex] > 0; // Mostrar el valor solo si es mayor que cero
+                                },
+                                color: 'black',
+                                align: 'top',
+                                formatter: function(value, context) {
+                                    return value; // Formatear el valor como desees
+                                }
+                            }
                         }
                     }
                 });
@@ -293,6 +184,7 @@ const muestraResultados = async () => {
         mostrarInfo(nota, color);
     }
 };
+
 
 
 
