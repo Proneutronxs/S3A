6bb15ec2def4a4648536e8929677793f6f57a7e7
Sides <fumanny@hotmail.com>
2024-10-02 11:44:36 -0300

    choferes

--- Applications/Api/urls.py
+++ Applications/Api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from Applications.Api import views
 from Applications.Api.vistas.CRC import demoCRC
+from Applications.Api.vistas.FruitTruck import fruit
 from S3A.funcionesGenerales import *
+
+
 urlpatterns = [
 
     path('data-general/', demoCRC.dataGeneral, name="data-general-api"),
@@ -10,7 +13,11 @@ urlpatterns = [
     
 
 
+    ### CAMIONES APLICACION DE FRUIT TRUCK ###
 
 
-    
+
+    path('listar-transporte/', fruit.listarTransporte, name="choferes_listar_transportes"),
+
+    path('listar-transporte/datos-transportes/', fruit.listarDataTransporte, name="choferes_listar_transportes_data_transportes"),
 ]
---
+++ Applications/Api/vistas/FruitTruck/fruit.py
@@ -0,0 +1,194 @@
+
+from django.views.decorators.csrf import csrf_exempt
+from S3A.funcionesGenerales import *
+from django.views.static import serve
+from django.db import connections
+from django.http import JsonResponse
+from django.http import HttpResponse, Http404
+import json
+
+
+def listarTransporte(request):
+    if request.method == 'GET':
+        try:
+            with connections['S3A'].cursor() as cursor:
+                sql = """ 
+                        SELECT IdTransportista, RTRIM(CONVERT(VARCHAR(23), RazonSocial)) AS RAZON_SOCIAL
+                        FROM Transportista 
+                        WHERE Activo ='S' 
+                        ORDER BY RazonSocial
+                    """
+                cursor.execute(sql)
+                consulta = cursor.fetchall()
+                if consulta:
+                    lista_data = []
+                    for row in consulta:
+                        idTransporte = str(row[0])
+                        razonSocial = str(row[1])
+                        datos = {'IdTransporte': idTransporte, 'RazonSocial': razonSocial}
+                        lista_data.append(datos)
+                    return JsonResponse({'Message': 'Success', 'Datos': lista_data})
+                else:
+                    return JsonResponse({'Message': 'Not Found', 'Nota': 'No se encontraron datos.'})
+        except Exception as e:
+            error = str(e)
+            insertar_registro_error_sql("API","LISTAR TRANSPORTES","GET",error)
+            return JsonResponse({'Message': 'Error', 'Nota': error})
+        finally:
+            connections['S3A'].close()
+    else:
+        return JsonResponse({'Message': 'No se pudo resolver la petición.'})
+    
+def listarDataTransporte(request):
+    if request.method == 'POST':
+        body = request.body.decode('utf-8')
+        idTransporte = str(json.loads(body)['IdTransporte'])
+        try:
+            with connections['S3A'].cursor() as cursor:
+
+                lista_chofer = []
+                lista_camion = []
+                lista_acoplado = []
+
+
+                sql = """ 
+                    SELECT IdChofer, ISNULL(RTRIM(Apellidos),'') + ' ' + ISNULL(RTRIM(Nombres),'') 
+                    FROM Chofer 
+                    WHERE IdTransportista = %s 
+                    ORDER BY Apellidos, Nombres
+                    """
+                cursor.execute(sql, [idTransporte])
+                consulta = cursor.fetchall()
+                if consulta:
+                    for row in consulta:
+                        idChofer = str(row[0])
+                        nombre = str(row[1])
+                        datos = {'IdChofer': idChofer, 'Chofer': nombre}
+                        lista_chofer.append(datos)
+
+                sql2 = """ 
+                    SELECT IdCamion, Descripcion = RTRIM(Nombre) + ' - ' + RTRIM(CONVERT(CHAR(6),Tara)) + ' - ' + RTRIM(Patente)
+                    FROM Camion 
+                    WHERE IdTransportista = %s 
+                    ORDER BY Nombre
+                    """
+                cursor.execute(sql2, [idTransporte])
+                consulta2 = cursor.fetchall()
+                if consulta2:
+                    for row2 in consulta2:
+                        idCamion = str(row2[0])
+                        camion = str(row2[1])
+                        datos = {'IdCamion': idCamion, 'Camion': camion}
+                        lista_camion.append(datos)
+
+                sql3 = """ 
+                    SELECT IdAcoplado, Descripcion = RTRIM(Nombre) + ' - ' + RTRIM(CONVERT(CHAR(6),Tara)) + ' - ' + RTRIM(Patente) 
+                    FROM Acoplado 
+                    WHERE IdTransportista = %s 
+                    ORDER BY Nombre
+                    """
+                cursor.execute(sql3, [idTransporte])
+                consulta3 = cursor.fetchall()
+                if consulta3:
+                    for row3 in consulta3:
+                        idAcoplado = str(row3[0])
+                        acoplado = str(row3[1])
+                        datos = {'IdAcoplado': idAcoplado, 'Acoplado': acoplado}
+                        lista_acoplado.append(datos)
+
+
+                if lista_chofer and lista_camion and lista_acoplado:
+                    return JsonResponse({'Message': 'Success', 'DataChofer': lista_chofer, 'DataCamion':lista_camion, 'DataAcoplado':lista_acoplado})
+                else:
+                    return JsonResponse({'Message': 'Not Found', 'Nota': 'No se encontraron datos.'})
+        except Exception as e:
+            error = str(e)
+            insertar_registro_error_sql("GeneralApp","personal_por_Ccostos_asistencia","usuario",error)
+            return JsonResponse({'Message': 'Error', 'Nota': error})
+        finally:
+            connections['S3A'].close()
+    else:
+        return JsonResponse({'Message': 'No se pudo resolver la petición.'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT IdTransportista, RTRIM(CONVERT(VARCHAR(23), RazonSocial)) AS RAZON_SOCIAL
+# FROM Transportista 
+# WHERE Activo ='S' 
+# ORDER BY RazonSocial
+
+# SELECT IdChofer, ISNULL(RTRIM(Apellidos),'') + ' ' + ISNULL(RTRIM(Nombres),'') 
+# FROM Chofer 
+# WHERE IdTransportista = 1000325 
+# ORDER BY Apellidos, Nombres
+
+# SELECT IdCamion, Descripcion = RTRIM(Nombre) + ' - ' + RTRIM(CONVERT(CHAR(6),Tara)) + ' - ' + RTRIM(Patente)
+# FROM Camion 
+# WHERE IdTransportista = 1000325 
+# ORDER BY Nombre
+
+# SELECT IdAcoplado, Descripcion = RTRIM(Nombre) + ' - ' + RTRIM(CONVERT(CHAR(6),Tara)) + ' - ' + RTRIM(Patente) 
+# FROM Acoplado 
+# WHERE IdTransportista = 1000325 
+# ORDER BY Nombre
