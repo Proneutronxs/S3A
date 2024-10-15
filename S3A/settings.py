e8b380e7095779f814a159044711856b7fe81ee0
Sides <fumanny@hotmail.com>
2024-10-15 11:52:59 -0300

    planillas-semana

--- S3A/settings.py
+++ S3A/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o@gi7gzjk8)i3m_gqsqu94$diq2^joa!8gr%1pi*td3e9bqi5_'
 
 # SECURITY WARNING: don't run with debug turned on in production! ##
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
