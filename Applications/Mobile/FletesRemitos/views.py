78c369fcab47aa0c1104c12cf64d50b4eacc37cd
Sides <fumanny@hotmail.com>
2025-01-23 16:54:49 -0300

    asignacion-cerrada

--- Applications/Mobile/FletesRemitos/views.py
+++ Applications/Mobile/FletesRemitos/views.py
@@ -373,19 +373,22 @@ def llamaAsignacionesPendientes(request, usuario):
         try:
             with connections['S3A'].cursor() as cursor:
                 sql = """ 
-                    SELECT        PedidoFlete.IdPedidoFlete AS ID, 
-                                CONVERT(VARCHAR,PedidoFlete.IdPedidoFlete) + ' - ' + RTRIM(Chacra.Nombre) AS ASIGNACION
+                     SELECT        PedidoFlete.IdPedidoFlete AS ID, 
+                                CONVERT(VARCHAR,PedidoFlete.IdPedidoFlete) + ' - ' + RTRIM(Chacra.Nombre) AS ASIGNACION, PedidoFlete.Estado
                     FROM            PedidoFlete 
-                    INNER JOIN      Chacra ON PedidoFlete.IdChacra = Chacra.IdChacra
+                                INNER JOIN      Chacra ON PedidoFlete.IdChacra = Chacra.IdChacra
                     WHERE        (PedidoFlete.UserID = %s) 
                                 AND (PedidoFlete.TipoCarga = 'RAU')
                                 AND (PedidoFlete.Estado = 'A') 
-                                --AND EXISTS (
-                                --SELECT 1 
-                                --FROM TRESASES_APLICATIVO.dbo.Datos_Remito_MovBins 
-                                --WHERE IdAsignacion = PedidoFlete.IdPedidoFlete 
-                                --    AND AsigCerrada IS NULL
-                                --)
+                                AND (EXISTS (
+                                    SELECT 1 
+                                    FROM TRESASES_APLICATIVO.dbo.Datos_Remito_MovBins 
+                                    WHERE IdAsignacion = PedidoFlete.IdPedidoFlete 
+                                        AND AsigCerrada IS NULL
+                                ) OR NOT EXISTS (
+                                    SELECT 1 
+                                    FROM TRESASES_APLICATIVO.dbo.Datos_Remito_MovBins 
+                                    WHERE IdAsignacion = PedidoFlete.IdPedidoFlete ))
                  """
                 cursor.execute(sql, [usuario])
                 consulta = cursor.fetchall()
